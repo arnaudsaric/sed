2134e783ff53a41ad91b9524b5395b19244ea123
Arnaud Saric <arnaud.saric@telecom-bretagne.eu>
2015-05-12 20:16:24 +0200

    [sed] Integrating regex modules into main program

--- src/sed.c
+++ src/sed.c
@@ -1,6 +1,7 @@
 #include "util.h"
 #include "compile.h"
 #include "module.h"
+#include "regex.h"
 #include <argp.h>
 
 typedef struct script {
@@ -82,6 +83,7 @@ typedef struct {
     script* scripts;
     in* ins;
     char* module;
+    char* regex_mod;
 } options;
 
 error_t option_parser (int key, char* arg, struct argp_state* state) {
@@ -93,6 +95,11 @@ error_t option_parser (int key, char* arg, struct argp_state* state) {
     if (key == 'M') {
         input->module = (char*) malloc(strlen(arg));
         strcpy(input->module, (const char*) arg);
+        return 0;
+    }
+    if (key == 'R') {
+        input->regex_mod = (char*) malloc(strlen(arg));
+        strcpy(input->regex_mod, (const char*) arg);
         return 0;
     }
     if (key == ARGP_KEY_ARG) {
@@ -139,12 +146,13 @@ error_t option_parser (int key, char* arg, struct argp_state* state) {
 }
 
 int main (int argc, char ** argv) {
-    const struct argp_option parser_options[6] = {
-        {.name = "quiet",      .key = 'n', .arg = NULL,          .flags = 0,            .doc = "No automatic printing of pattern space"},
-        {.name = "silent",     .key = 'n', .arg = NULL,          .flags = OPTION_ALIAS, .doc = NULL},
-        {.name = "expression", .key = 'e', .arg = "script",      .flags = 0,            .doc = "A script string to be executed"},
-        {.name = "file",       .key = 'f', .arg = "script-file", .flags = 0,            .doc = "A file containing a script to be executed"},
-        {.name = "module",     .key = 'M', .arg = "module-file", .flags = 0,            .doc = "An msed module"}
+    const struct argp_option parser_options[7] = {
+        {.name = "quiet",      .key = 'n', .arg = NULL,                .flags = 0,            .doc = "No automatic printing of pattern space"},
+        {.name = "silent",     .key = 'n', .arg = NULL,                .flags = OPTION_ALIAS, .doc = NULL},
+        {.name = "expression", .key = 'e', .arg = "script",            .flags = 0,            .doc = "A script string to be executed"},
+        {.name = "file",       .key = 'f', .arg = "script-file",       .flags = 0,            .doc = "A file containing a script to be executed"},
+        {.name = "module",     .key = 'M', .arg = "module-file",       .flags = 0,            .doc = "(mandatory) An msed module"},
+        {.name = "regex",      .key = 'R', .arg = "regex-module-file", .flags = 0,            .doc = "(mandatory) A regex module"}
     };
     const struct argp_child child = {0};
     struct argp parser = {
@@ -157,6 +165,7 @@ int main (int argc, char ** argv) {
         .argp_domain = NULL,
     };
     module* mod;
+    regex_module* regex_mod;
     options opts = {0};
     argp_parse(&parser, argc, argv, 0, NULL, &opts);
     if (!opts.module) {
@@ -164,6 +173,12 @@ int main (int argc, char ** argv) {
         execl(argv[0], argv[0], "--help", NULL);
         exit(EXIT_FAILURE);
     }
+    if (!opts.regex_mod) {
+        fprintf(stderr, "No regex module specified! Here is usage:\n");
+        execl(argv[0], argv[0], "--help", NULL);
+        exit(EXIT_FAILURE);
+    }
+    init_regex(&regex_mod, opts.regex_mod, false);
     init_mod(&mod, opts.module, !opts.scripts);
     if (!opts.ins)
         exit(sed(mod, opts.scripts, stdin, stdout, opts.quiet));
