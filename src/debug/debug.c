b305a89f9bd2fbde747cd60c412fdd4f0432d2e6
Arnaud Saric <arnaud.saric@telecom-bretagne.eu>
2015-05-12 21:22:45 +0200

    [debug] Adding regex module to debug

--- src/debug/debug.c
+++ src/debug/debug.c
@@ -1,9 +1,12 @@
 #include "compile.h"
 #include "module.h"
+#include "regex.h"
 
 int main (int argc, char** argv) {
     module* mod_posix;
-    init_mod(&mod_posix, "modules/posix.so");
+    regex_module* regex_mod;
+    init_regex(&regex_mod, "regex/posix.so", false);
+    init_mod(&mod_posix, "modules/posix.so", false, regex_mod);
     token* first = compile(mod_posix->commands, argv[1]);
     token* tok = first;
     for (;tok;tok = tok->nexttoken) {
